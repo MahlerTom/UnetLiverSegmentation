e01413297dcfbe2c669cc2c81598c2feffff2e10
Tom Mahler <tom@mahler.tech>
2019-06-01 14:46:28 +0300

    changed test names

--- rename_files.py
+++ rename_files.py
@@ -8,8 +8,8 @@ Created on Sat May 25 16:45:04 2019
 import os
 from tqdm import tqdm
 import shutil
-src = 'data/val/images/'
-dst = 'data/val/images2/'
+src = 'data/test/images/'
+dst = 'data/test/images2/'
 
 for d in tqdm(os.listdir(src)):
     shutil.copyfile(src + d, dst + d[3:])
