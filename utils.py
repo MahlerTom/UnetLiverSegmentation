6316b10ec49b48a87d53ef898f5c18b522e9089b
Tom Mahler <tom@mahler.tech>
2019-06-07 18:35:23 +0300

    removed export_pred_test from utils
    only works with jupyter notebook

--- utils.py
+++ utils.py
@@ -25,31 +25,3 @@ def prepare_tumor(y_true, y_pred):
     y_pred_tumor = replace_tensor(y_pred_tumor, 0.5, 0.0)
     y_true_tumor = replace_tensor(y_true, 0.5, 0.0)
     return y_true_tumor, y_pred_tumor
-
-def export_pred_test(
-    model,
-    export_folder_name,
-    test_path,
-    images_paths,
-    export_path='/content',
-    ds=None,
-    steps=None,
-    verbose=1,
-    images_to_print=0,
-    norm=255.0,
-    _resize=[256, 256]
-):
-    save_image_path = os.path.join(test_path, export_folder_name)
-    export_image_path = f'{os.path.join(export_path, export_folder_name)}.zip'
-    print(save_image_path, export_image_path)
-    !{'rm -r ' + export_image_path}
-    !{'rm -r ' + save_image_path}
-    !{'mkdir ' + save_image_path}
-
-    pred = model_predict(model, ds=ds, steps=steps, images_paths=images_paths,
-                        masks_paths=None,
-                        verbose=verbose, images_to_print=images_to_print, norm=norm,
-                        _resize=_resize, save_image_path=save_image_path)
-
-    !{'zip -r ' + export_image_path + ' ' + save_image_path}
-    return pred
