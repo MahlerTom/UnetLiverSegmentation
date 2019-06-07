3554e43a8f60b66b59aa2f2c7e33954f98825ed6
Tom Mahler <tom@mahler.tech>
2019-06-07 18:16:49 +0300

    added model_predict to model_ops

--- model_ops.py
+++ model_ops.py
@@ -1,15 +1,21 @@
 from .model import unet
 from .losses import dice_coef_loss
 from .metrics import dice_coef, dice_coef_liver, dice_coef_tumor
-from .data import init_ds
+from .data import init_ds, image_name
+from .utils import quantizatize
 
 import datetime
 import time
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import TensorBoard, EarlyStopping
 from tensorflow.keras.metrics import Precision, Recall
-from numpy.random import seed
 from tensorflow.random import set_seed
+import numpy as np
+from numpy.random import seed
+import matplotlib.pyplot as plt
+from skimage import io
+from PIL import Image
+
 
 def model_fit(
     table=None,
@@ -101,3 +107,73 @@ def model_fit(
         model.save_weights(f'{run_name}.h5')
     print(f'--- {(time.time() - start_time):.2f} seconds ---')
     return model, table
+
+def model_predict(
+    model,
+    images_paths,
+    masks_paths,
+    ds=None,
+    steps=None,     
+    verbose=1,
+    images_to_print=10,
+    norm=255.0,
+    _resize=[256, 256], # _resize=[128, 128]
+    save_image_path=None,
+):
+    from skimage import io
+    if steps is None:
+        steps = len(images_paths)
+    
+    ds = init_ds(
+        ds, 
+        images_paths=images_paths, masks_paths=masks_paths, 
+        norm=norm, _resize=_resize, batch_size=1, shuffle=False, verbose=verbose)
+    
+    pred = model.predict(ds, verbose=verbose, steps=steps)
+    print(pred.shape)
+
+    if images_to_print is None:
+        images_to_print = len(images_paths)
+
+    num_of_splt = 4
+    splt = 1
+    plt.figure(figsize=(num_of_splt*6, images_to_print*6))
+    if save_image_path is not None:
+        for i in range(len(images_paths)):
+            Image.fromarray(quantizatize(np.squeeze(pred[i,:,:,:]), 3, 170.0)*255).convert('L').save(os.path.join(save_image_path, f'{image_name(images_paths[i])}.png'))
+      
+    for i in range(images_to_print):
+        i_pred = np.squeeze(pred[i,:,:,:])
+
+        plt.subplot(images_to_print, num_of_splt, splt)
+        plt.imshow(i_pred, cmap='gray', vmin=0, vmax=1)
+        plt.title(f'{i} pred {image_name(images_paths[i])}')
+        plt.grid(False); plt.xticks([]); plt.yticks([])
+
+        plt.subplot(images_to_print, num_of_splt, splt+1)
+        plt.imshow(quantizatize(i_pred, 3, 170.0), cmap='gray', vmin=0, vmax=1)
+        plt.title(f'{i} quant {image_name(images_paths[i])}')
+        plt.grid(False); plt.xticks([]); plt.yticks([])
+
+        if masks_paths is not None:
+            plt.subplot(images_to_print, num_of_splt, splt+2)
+            plt.imshow(io.imread(masks_paths[i], as_gray=True), cmap='gray', vmin=0, vmax=1)
+            plt.title(f'{i} val {image_name(masks_paths[i])}')
+            plt.grid(False); plt.xticks([]); plt.yticks([])    
+            
+        plt.subplot(images_to_print, num_of_splt, splt+3)
+        plt.imshow(io.imread(images_paths[i], as_gray=True), cmap='gray', vmin=0, vmax=1)
+        plt.title(f'{i} original {image_name(images_paths[i])}')
+        plt.grid(False); plt.xticks([]); plt.yticks([])
+
+        splt += num_of_splt
+    
+    plt.show()
+    return pred
+
+def model_evaluate(model, ds=None, images_paths=val_images_paths, masks_paths=val_masks_paths, norm=255.0, _resize=[256,256], verbose=1): 
+    ds = init_ds(
+        ds, 
+        images_paths=images_paths, masks_paths=masks_paths, 
+        norm=norm, _resize=_resize, batch_size=1, shuffle=False)    
+    return model.evaluate(ds, steps=len(images_paths), verbose=verbose)
